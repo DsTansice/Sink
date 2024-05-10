9e4c5cf7aea6bfc04544759b1af244950bdd4919
ccbikai <ccbikai@gmail.com>
2024-05-10 21:18:16 +0800

    feat: stat

--- server/utils/cloudflare.ts
+++ server/utils/cloudflare.ts
@@ -1,8 +1,8 @@
 import type { H3Event } from 'h3'
 
 export function useWAE(event: H3Event, query: string) {
-  const { cfAccountId, cfApiToken } = useRuntimeConfig()
-  console.log(query)
+  const { cfAccountId, cfApiToken } = useRuntimeConfig(event)
+  console.log(query, useRuntimeConfig(event), useRuntimeConfig())
   return $fetch(`https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/analytics_engine/sql`, {
     method: 'POST',
     headers: {
