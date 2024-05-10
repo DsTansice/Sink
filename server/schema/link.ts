0bb8eb37fa1e61057197906d48d80399e36ed0da
ccbikai <ccbikai@gmail.com>
2024-05-10 16:06:58 +0800

    feat: regex

--- server/schema/link.ts
+++ server/schema/link.ts
@@ -4,7 +4,7 @@ const { slugRegex } = useAppConfig()
 
 export const LinkSchema = z.object({
   id: z.string().trim().max(26).default(nanoid(10)),
-  slug: z.string().trim().regex(slugRegex).max(2048).default(nanoid(+useRuntimeConfig().slugDefaultLength)),
+  slug: z.string().trim().regex(new RegExp(slugRegex)).max(2048).default(nanoid(+useRuntimeConfig().slugDefaultLength)),
   url: z.string().trim().url().max(2048),
   createdAt: z.number().int().safe().default(() => Math.floor(Date.now() / 1000)),
   updatedAt: z.number().int().safe().default(() => Math.floor(Date.now() / 1000)),
