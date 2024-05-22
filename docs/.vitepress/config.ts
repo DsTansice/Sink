dd2111001608ef0d5a29bbcf8436bacc0c2c33fa
ccbikai <ccbikai@gmail.com>
2024-05-22 20:15:06 +0800

    feat: docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,21 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Sink Docs',
   description: 'Sink is a Simple Link Shortener with Analytics, 100% run on Cloudflare.',
+  locales: {
+    root: {
+      label: 'English',
+      lang: 'en',
+    },
+    zh: {
+      label: '中文',
+      lang: 'zh',
+      link: '/zh/',
+    },
+  },
   themeConfig: {
+    search: {
+      provider: 'local',
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Sink', link: 'https://sink.cool' },
@@ -23,6 +37,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
+      { icon: 'twitter', link: 'https://x.com/ccbikai' },
       { icon: 'github', link: 'https://github.com/ccbikai/sink' },
     ],
   },
